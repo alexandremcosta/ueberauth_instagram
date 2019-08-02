524639dda067af91ffc3e80b35a70e7915be2d9f
Alexandre Costa <alexandremcost@gmail.com>
2019-08-02 15:59:57 -0700

    bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule UeberauthInstagram.Mixfile do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.2.1"
   @url "https://github.com/jasontruluck/ueberauth_instagram"
 
   def project do
